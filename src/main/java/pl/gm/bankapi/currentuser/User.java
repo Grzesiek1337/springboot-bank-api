a52dbc8318f5365d81451cd0186cbcd3227219df
Grzesiek1337 <gmiedzian@gmail.com>
2022-09-22 21:29:48 +0200

    initial commit

---
+++ src/main/java/pl/gm/bankapi/currentuser/User.java
@@ -0,0 +1,44 @@
+package pl.gm.bankapi.currentuser;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    @NotBlank
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$",message = "Incorrect email.")
+    private String username;
+    @NotBlank
+    @Size(min = 3,max=30)
+    private String password;
+    private String role;
+    private boolean enabled;
+//    @OneToOne(cascade = { CascadeType.REMOVE, CascadeType.MERGE })
+//    @JoinColumn(name = "bankaccount_id")
+//    private BankAccount bankAccount;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/pl/gm/bankapi/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package pl.gm.bankapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pl.gm.bankapi.currentuser.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User getUserByUsername(@Param("username") String username);
+
+
+}
