77e017a331b834410bcbf3623caafc6ccf2463f2
Grzesiek1337 <gmiedzian@gmail.com>
2022-09-26 16:41:51 +0200

    user accounts model attribute

--- src/main/java/pl/gm/bankapi/controllers/BankController.java
+++ src/main/java/pl/gm/bankapi/controllers/BankController.java
@@ -1,12 +1,23 @@
 package pl.gm.bankapi.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.gm.bankapi.currentuser.CurrentUserDetails;
+import pl.gm.bankapi.model.BankAccount;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/bank-api")
 public class BankController {
+
+    @ModelAttribute("accounts")
+    public List<BankAccount> userBankAccounts(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
+        return currentUserDetails.getUser().getBankAccounts();
+    }
 
     @GetMapping
     public String getBankAccountIndex() {
