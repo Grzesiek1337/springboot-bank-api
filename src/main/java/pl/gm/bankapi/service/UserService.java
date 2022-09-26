e9e8731df3e00da1193fd8495dcc04958d68e478
Grzesiek1337 <gmiedzian@gmail.com>
2022-09-26 20:14:33 +0200

    method getting all user names, create user method

--- src/main/java/pl/gm/bankapi/service/UserService.java
+++ src/main/java/pl/gm/bankapi/service/UserService.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.gm.bankapi.currentuser.User;
+import pl.gm.bankapi.model.BankClient;
 import pl.gm.bankapi.repositories.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -14,13 +18,22 @@ public class UserService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User createTestUser() {
-        User user = new User();
-        user.setUsername("gm116@gmail.com");
-        user.setPassword(bCryptPasswordEncoder.encode("aaa"));
-        user.setRole("ROLE_USER");
-        user.setEnabled(true);
-        userRepository.save(user);
-        return user;
+    public List<String> getAllUserNames() {
+        return userRepository.findAll().stream()
+                .map(User::getUsername)
+                .collect(Collectors.toList());
     }
+
+    public void createUserFromClient(BankClient bankClient) {
+        User user = new User();
+        user.setUsername(bankClient.getClientEmail());
+        user.setPassword(bCryptPasswordEncoder.encode(bankClient.getClientPassword()));
+        user.setEnabled(true);
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+
+
+    }
+
+
 }
