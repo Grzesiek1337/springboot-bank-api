a554ea77061092e857b1957cdcdc81a4660c7465
Grzesiek1337 <gmiedzian@gmail.com>
2022-09-26 20:16:25 +0200

    client create methods

--- src/main/java/pl/gm/bankapi/controllers/UserController.java
+++ src/main/java/pl/gm/bankapi/controllers/UserController.java
@@ -2,47 +2,46 @@ package pl.gm.bankapi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import pl.gm.bankapi.currentuser.CurrentUserDetails;
 import pl.gm.bankapi.currentuser.User;
 import pl.gm.bankapi.model.BankAccount;
+import pl.gm.bankapi.model.BankClient;
 import pl.gm.bankapi.model.SavingAccount;
 import pl.gm.bankapi.model.StandardAccount;
 import pl.gm.bankapi.service.UserService;
 
+
+import javax.validation.Valid;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/user-test-create")
-    public User createTestUser() {
-        return userService.createTestUser();
-    }
-
     @GetMapping("/whoami")
     public String whoAmI(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
         return currentUserDetails.getUser().toString();
     }
 
-    @GetMapping("/set-accounts")
-    @ResponseBody
-    public String setAccounts(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
-        StandardAccount standardAccount = new StandardAccount(null, "1234", 100.25);
-        SavingAccount savingAccount = new SavingAccount(null, "5678", 0.00,7);
-        User user = currentUserDetails.getUser();
-        List<BankAccount> accounts = user.getBankAccounts();
-        accounts.add(standardAccount);
-        accounts.add(savingAccount);
-        user.setBankAccounts(accounts);
-        return accounts.toString();
+    @GetMapping("/create")
+    public String createClient(Model model) {
+        model.addAttribute("client", new BankClient());
+        return "client/create";
+    }
 
+    @PostMapping("/create")
+    public String saveClientAsUser(@ModelAttribute("client") @Valid BankClient bankClient, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "client/create";
+        }
+        userService.createUserFromClient(bankClient);
+        return "index";
     }
 }
