8faa28c6fecaec5041967d13afdd56e42a2a33d7
Grzesiek1337 <gmiedzian@gmail.com>
2022-09-26 20:18:50 +0200

    edited onstructor

--- src/main/java/pl/gm/bankapi/model/StandardAccount.java
+++ src/main/java/pl/gm/bankapi/model/StandardAccount.java
@@ -5,7 +5,7 @@ import lombok.*;
 @Getter
 @Setter
 public class StandardAccount extends BankAccount {
-    public StandardAccount(Long id, String accountNumber, double accountBalance) {
-        super(id, accountNumber, accountBalance);
+    public StandardAccount(Long id, String accountType, String accountNumber, double accountBalance) {
+        super(id, accountType, accountNumber, accountBalance);
     }
 }
