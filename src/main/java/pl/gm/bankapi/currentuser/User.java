c0a8099a9798fa04a9d0a001eeb32fe3fb88fdde
Grzesiek1337 <gmiedzian@gmail.com>
2022-09-26 20:17:45 +0200

    removed validation annotations

--- src/main/java/pl/gm/bankapi/currentuser/User.java
+++ src/main/java/pl/gm/bankapi/currentuser/User.java
@@ -7,9 +7,6 @@ import lombok.Setter;
 import pl.gm.bankapi.model.BankAccount;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
@@ -25,25 +22,19 @@ public class User {
     private Long id;
 
     @Column(unique = true)
-    @NotBlank
-    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$",message = "Incorrect email.")
     private String username;
-    @NotBlank
-    @Size(min = 3,max=255)
     private String password;
     private String role;
     private boolean enabled;
     @OneToMany(cascade = { CascadeType.REMOVE, CascadeType.MERGE },fetch = FetchType.EAGER)
     @JoinColumn(name = "bankaccount_id")
     private List<BankAccount> bankAccounts;
-
     public User(String username, String password, String role, boolean enabled) {
         this.username = username;
         this.password = password;
         this.role = role;
         this.enabled = enabled;
     }
-
     @Override
     public String toString() {
         return "User{" +
