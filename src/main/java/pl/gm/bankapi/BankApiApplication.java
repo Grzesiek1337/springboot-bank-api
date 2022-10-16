2f28a70f515a28f35c74b7e2a8876ea119c158e0
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-16 21:21:05 +0200

    component scan annotation, model mapper bean

--- src/main/java/pl/gm/bankapi/BankApiApplication.java
+++ src/main/java/pl/gm/bankapi/BankApiApplication.java
@@ -1,14 +1,17 @@
 package pl.gm.bankapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
 
 @SpringBootApplication
+@ComponentScan("pl.gm")
 public class BankApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -18,6 +21,11 @@ public class BankApiApplication implements CommandLineRunner {
 	@Bean
 	public Validator validator() {
 		return new LocalValidatorFactoryBean();
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 	@Override
