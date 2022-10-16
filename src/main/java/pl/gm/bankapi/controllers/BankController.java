4af23b8779328f2d2ee486b829257bd8a7f3261a
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-16 21:24:13 +0200

    redundant code removed

--- src/main/java/pl/gm/bankapi/controllers/BankController.java
+++ src/main/java/pl/gm/bankapi/controllers/BankController.java
@@ -1,26 +1,17 @@
 package pl.gm.bankapi.controllers;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.gm.bankapi.currentuser.CurrentUserDetails;
-import pl.gm.bankapi.model.BankAccount;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/bank-api")
+@AllArgsConstructor
 public class BankController {
 
-    @ModelAttribute("accounts")
-    public List<BankAccount> userBankAccounts(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
-        return currentUserDetails.getUser().getBankAccounts();
-    }
-
     @GetMapping
-    public String getBankAccountIndex() {
+    public String getBankAccountIndexPage() {
         return "bank-account";
     }
 }
