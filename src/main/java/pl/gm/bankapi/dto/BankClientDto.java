3a4617596d6319026971f5edb24dd912b7af1939
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-16 21:22:32 +0200

    class for the form impl

---
+++ src/main/java/pl/gm/bankapi/dto/BankClientDto.java
@@ -0,0 +1,34 @@
+package pl.gm.bankapi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class BankClientDto {
+    @Column(unique = true)
+    @NotBlank(message = "Not blank")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$",message = "Incorrect email address.")
+    private String clientEmail;
+    @NotBlank(message = "Not blank")
+    @Size(min = 3,max=255, message = "Incorrect password size.")
+    private String clientPassword;
+    @NotBlank(message = "Not blank")
+    @Size(min = 3,max=20, message = "Incorrect first name size.")
+    private String clientFirstName;
+    @NotBlank(message = "Not blank")
+    @Size(min = 3,max=20, message = "Incorrect last name size.")
+    private String clientLastName;
+    @NotNull
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private LocalDate clientDayOfBirth;
+}
+
