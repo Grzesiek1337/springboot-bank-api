86bf48b4976095f0672be4a78cbce3fd3724bcf2
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-18 18:16:03 +0200

    import path change

--- src/main/java/pl/gm/bankapi/repositories/UserRepository.java
+++ src/main/java/pl/gm/bankapi/repositories/UserRepository.java
@@ -4,7 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import pl.gm.bankapi.currentuser.User;
+import pl.gm.bankapi.model.User;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
