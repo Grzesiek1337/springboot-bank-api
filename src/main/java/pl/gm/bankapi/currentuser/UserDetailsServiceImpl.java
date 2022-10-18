7b93b6b0470dc175a005ec6eb2993154a0768ceb
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-18 18:39:20 +0200

    class import

--- src/main/java/pl/gm/bankapi/currentuser/UserDetailsServiceImpl.java
+++ src/main/java/pl/gm/bankapi/currentuser/UserDetailsServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.gm.bankapi.model.User;
 import pl.gm.bankapi.repositories.UserRepository;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
