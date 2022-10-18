5d69edfbf8e3dc3aa2117f03f6083529627b741b
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-18 18:30:59 +0200

    entity class

--- src/main/java/pl/gm/bankapi/model/StandardAccount.java
+++ src/main/java/pl/gm/bankapi/model/StandardAccount.java
@@ -1,11 +1,28 @@
 package pl.gm.bankapi.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
-public class StandardAccount extends BankAccount {
-    public StandardAccount(Long id, String accountType, String accountNumber, double accountBalance) {
-        super(id, accountType, accountNumber, accountBalance);
-    }
+@NoArgsConstructor
+@Entity
+@Table(name = "standard_accounts")
+public class StandardAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String accountType;
+    @Column(unique = true)
+    private String accountNumber;
+    private double accountBalance;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+
 }
