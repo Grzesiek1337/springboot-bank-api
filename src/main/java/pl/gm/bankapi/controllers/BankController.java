7d80421cb7acd7435e2925403fc10051909a852e
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-18 19:08:12 +0200

    model attribute

--- src/main/java/pl/gm/bankapi/controllers/BankController.java
+++ src/main/java/pl/gm/bankapi/controllers/BankController.java
@@ -1,14 +1,26 @@
 package pl.gm.bankapi.controllers;
 
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.gm.bankapi.currentuser.CurrentUserDetails;
+import pl.gm.bankapi.dto.StandardAccountDto;
 
 @Controller
 @RequestMapping("/bank-api")
 @AllArgsConstructor
 public class BankController {
+
+    private ModelMapper modelMapper;
+
+    @ModelAttribute("account")
+    public StandardAccountDto standardAccount(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
+        return modelMapper.map(currentUserDetails.getUser().getClient().getStandardAccount(),StandardAccountDto.class);
+    }
 
     @GetMapping
     public String getBankAccountIndexPage() {
