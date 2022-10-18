c3a99e6da1da853b8a5437a0c056248729eafdcc
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-18 18:07:56 +0200

    File location changed

---
+++ src/main/java/pl/gm/bankapi/model/User.java
@@ -0,0 +1,28 @@
+package pl.gm.bankapi.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String role;
+    private boolean enabled;
+
+    @OneToOne(mappedBy = "user")
+    private Client client;
+
+}
