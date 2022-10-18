048557391c738a9daef01481f09f4fe35fea9114
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-18 18:20:30 +0200

    first implementation of the user creation method

--- src/main/java/pl/gm/bankapi/service/UserService.java
+++ src/main/java/pl/gm/bankapi/service/UserService.java
@@ -1,39 +1,50 @@
 package pl.gm.bankapi.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import pl.gm.bankapi.currentuser.User;
-import pl.gm.bankapi.model.BankClient;
+import pl.gm.bankapi.model.User;
+import pl.gm.bankapi.dto.BankClientDto;
+import pl.gm.bankapi.dto.ClientDto;
+import pl.gm.bankapi.dto.StandardAccountDto;
+import pl.gm.bankapi.generator.BankAccountNumberGenerator;
+import pl.gm.bankapi.model.Client;
+import pl.gm.bankapi.repositories.ClientRepository;
 import pl.gm.bankapi.repositories.UserRepository;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.transaction.Transactional;
 
 @Service
+@AllArgsConstructor
 public class UserService {
 
-    @Autowired
     private UserRepository userRepository;
-    @Autowired
+    private ClientService clientService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private ModelMapper modelMapper;
 
-    public List<String> getAllUserNames() {
-        return userRepository.findAll().stream()
-                .map(User::getUsername)
-                .collect(Collectors.toList());
-    }
+    @Transactional
+    public void createClient(BankClientDto bankClient) throws Exception {
+        StandardAccountDto standardAccount = new StandardAccountDto();
+        standardAccount.setAccountType("Standard");
+        standardAccount.setAccountBalance(0.00);
+        standardAccount.setAccountNumber(BankAccountNumberGenerator.createAccountNumber());
 
-    public void createUserFromClient(BankClient bankClient) {
         User user = new User();
         user.setUsername(bankClient.getClientEmail());
         user.setPassword(bCryptPasswordEncoder.encode(bankClient.getClientPassword()));
         user.setEnabled(true);
         user.setRole("ROLE_USER");
-        userRepository.save(user);
+        userRepository.save(modelMapper.map(user,User.class));
 
-
+        ClientDto client = new ClientDto();
+        client.setFirstName(bankClient.getClientFirstName());
+        client.setLastName(bankClient.getClientLastName());
+        client.setDayOfBirth(bankClient.getClientDayOfBirth());
+        client.setUser(user);
+        client.setStandardAccount(standardAccount);
+        standardAccount.setClient(client);
+        clientService.save(modelMapper.map(client, Client.class));
     }
-
-
 }
