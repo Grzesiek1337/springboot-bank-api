0ea3e8e7bededb0f567db9e164b06e1527d60a82
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-18 18:12:28 +0200

    exception handling

--- src/main/java/pl/gm/bankapi/controllers/UserController.java
+++ src/main/java/pl/gm/bankapi/controllers/UserController.java
@@ -1,47 +1,39 @@
 package pl.gm.bankapi.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import pl.gm.bankapi.currentuser.CurrentUserDetails;
-import pl.gm.bankapi.currentuser.User;
-import pl.gm.bankapi.model.BankAccount;
-import pl.gm.bankapi.model.BankClient;
-import pl.gm.bankapi.model.SavingAccount;
-import pl.gm.bankapi.model.StandardAccount;
+
+import pl.gm.bankapi.dto.BankClientDto;
 import pl.gm.bankapi.service.UserService;
 
-
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/user")
+@AllArgsConstructor
 public class UserController {
 
-    @Autowired
     private UserService userService;
-
-    @GetMapping("/whoami")
-    public String whoAmI(@AuthenticationPrincipal CurrentUserDetails currentUserDetails) {
-        return currentUserDetails.getUser().toString();
-    }
-
     @GetMapping("/create")
     public String createClient(Model model) {
-        model.addAttribute("client", new BankClient());
+        model.addAttribute("client", new BankClientDto());
         return "client/create";
     }
 
     @PostMapping("/create")
-    public String saveClientAsUser(@ModelAttribute("client") @Valid BankClient bankClient, BindingResult bindingResult, Model model) {
-        if(bindingResult.hasErrors()) {
+    public String saveClient(@ModelAttribute("client") @Valid BankClientDto bankClient, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "client/create";
         }
-        userService.createUserFromClient(bankClient);
-        return "index";
+        try {
+            userService.createClient(bankClient);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
     }
 }
