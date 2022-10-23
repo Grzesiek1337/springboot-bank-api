6bd41f093f98d336caaf4aa28fff420a08365f46
Grzesiek1337 <gmiedzian@gmail.com>
2022-10-23 17:43:57 +0200

    testing change

--- src/main/java/pl/gm/bankapi/controllers/IndexController.java
+++ src/main/java/pl/gm/bankapi/controllers/IndexController.java
@@ -1,15 +1,22 @@
 package pl.gm.bankapi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.gm.bankapi.repositories.FakeStandardAccountRepo;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
+    @Autowired
+    private FakeStandardAccountRepo fakeStandardAccountRepo;
+
     @GetMapping
+    @ResponseBody
     public String startIndex() {
-        return "index";
+         return fakeStandardAccountRepo.getAccounts().toString();
     }
 }
