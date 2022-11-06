e97823ec15788f58f1d183a43cc5998a9a77b085
Grzesiek1337 <gmiedzian@gmail.com>
2022-11-06 18:01:05 +0100

    index

--- src/main/java/pl/gm/bankapi/controllers/IndexController.java
+++ src/main/java/pl/gm/bankapi/controllers/IndexController.java
@@ -1,22 +1,15 @@
 package pl.gm.bankapi.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import pl.gm.bankapi.repositories.FakeStandardAccountRepo;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
-    @Autowired
-    private FakeStandardAccountRepo fakeStandardAccountRepo;
-
     @GetMapping
-    @ResponseBody
-    public String startIndex() {
-         return fakeStandardAccountRepo.getAccounts().toString();
+    String getIndex() {
+         return "index";
     }
 }
