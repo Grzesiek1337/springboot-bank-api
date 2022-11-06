8494af2e0427b9faa1c8da216c1846a699ea3b01
Grzesiek1337 <gmiedzian@gmail.com>
2022-11-06 18:06:45 +0100

    my changes

--- src/main/java/pl/gm/bankapi/BankApiApplication.java
+++ src/main/java/pl/gm/bankapi/BankApiApplication.java
@@ -1,14 +1,21 @@
 package pl.gm.bankapi;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import pl.gm.bankapi.generator.BankAccountNumberGenerator;
+import pl.gm.bankapi.model.Money;
+import pl.gm.bankapi.model.StandardAccount;
+import pl.gm.bankapi.repositories.StandardAccountRepository;
+import pl.gm.bankapi.service.MoneyService;
 
 import javax.validation.Validator;
+import java.math.BigDecimal;
 
 @SpringBootApplication
 @ComponentScan("pl.gm")
@@ -28,8 +35,25 @@ public class BankApiApplication implements CommandLineRunner {
 		return new ModelMapper();
 	}
 
+	@Autowired
+	private MoneyService moneyService;
+	@Autowired
+	private StandardAccountRepository standardAccountRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+		StandardAccount standardAccount = new StandardAccount();
+		standardAccount.setAccountType("Standard");
+		standardAccount.setAccountNumber(BankAccountNumberGenerator.createAccountNumber());
+		Money money = new Money();
+		money.setId(null);
+		money.setAmount(new BigDecimal("0.00"));
+		money.setStandardAccount(standardAccount);
+		money.setStandardAccount(standardAccount);
+		standardAccount.setAccountMoney(money);
+		standardAccountRepository.save(standardAccount);
+		moneyService.save(money);
+
 
 	}
 }
