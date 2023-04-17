0f1e319a76b9cec952018497cf45318f65676908
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-17 17:04:42 +0200

    new method: findByAccountNumber + query descriptions

--- src/main/java/pl/gm/bankapi/account/repository/BankAccountRepository.java
+++ src/main/java/pl/gm/bankapi/account/repository/BankAccountRepository.java
@@ -11,9 +11,30 @@ import java.util.List;
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount,Long> {
 
+    /**
+     * Retrieves all bank accounts belonging to the client with the specified ID.
+     *
+     * @param clientId the ID of the client
+     * @return a list of bank accounts belonging to the client
+     */
     @Query("SELECT b FROM BankAccount b WHERE b.client.id = :clientId")
     List<BankAccount> getAccountsByClientId(@Param("clientId") Long clientId);
 
+    /**
+     * Retrieves all bank accounts belonging to the user with the specified username.
+     *
+     * @param username the username of the user
+     * @return a list of bank accounts belonging to the user
+     */
     @Query("SELECT b FROM BankAccount b JOIN b.client c JOIN c.user u WHERE u.username = :username")
     List<BankAccount> getAccountsByUsername(@Param("username") String username);
+
+    /**
+     * Retrieves the bank account with the specified account number.
+     *
+     * @param accountNumber the account number of the bank account
+     * @return the bank account with the specified account number
+     */
+    @Query("SELECT b FROM BankAccount b WHERE b.accountNumber = :accountNumber")
+    BankAccount findByAccountNumber(@Param("accountNumber") String accountNumber);
 }
