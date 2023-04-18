beeb11461719fd93b5fd358493f6d7ff53b6f114
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-18 17:31:25 +0200

    new impl: transfer form,validation,method

--- src/main/java/pl/gm/bankapi/transfer/controller/TransferController.java
+++ src/main/java/pl/gm/bankapi/transfer/controller/TransferController.java
@@ -1,23 +1,33 @@
 package pl.gm.bankapi.transfer.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import pl.gm.bankapi.account.service.BankAccountService;
 import pl.gm.bankapi.money.Money;
+import pl.gm.bankapi.transfer.dto.TransferDto;
 import pl.gm.bankapi.transfer.exception.InsufficientFundsException;
 import pl.gm.bankapi.transfer.exception.InvalidAccountNumberException;
 import pl.gm.bankapi.transfer.service.TransferService;
+import pl.gm.bankapi.user.currentuser.CurrentUserDetails;
 
+import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.Currency;
 
-@RestController
+@Controller
 @RequestMapping("/transfer")
 public class TransferController {
 
     private final TransferService transferService;
+    private final BankAccountService bankAccountService;
 
-    public TransferController(TransferService transferService) {
+    public TransferController(TransferService transferService, BankAccountService bankAccountService) {
         this.transferService = transferService;
+        this.bankAccountService = bankAccountService;
     }
 
     /**
@@ -29,6 +39,7 @@ public class TransferController {
      * or a bad request message with an error message if the transfer failed.
      */
     @GetMapping("/{from}/{to}/{amount}")
+    @ResponseBody
     public ResponseEntity<String> transferMoney(
             @PathVariable("from") String fromAccountNumber,
             @PathVariable("to") String toAccountNumber,
@@ -37,10 +48,44 @@ public class TransferController {
         Currency currency = Currency.getInstance("PLN");
         Money money = new Money(amount, currency);
         try {
-            transferService.transferMoney(fromAccountNumber, toAccountNumber, money, currency);
+            transferService.transferMoney(fromAccountNumber, toAccountNumber, money);
             return ResponseEntity.ok("Money transferred successfully");
         } catch (InsufficientFundsException | InvalidAccountNumberException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/new/{senderAccountNumber}")
+    public String showTransferForm(
+            @PathVariable String senderAccountNumber,
+            @AuthenticationPrincipal CurrentUserDetails currentUserDetails,
+            Model model) {
+        TransferDto transferDto = new TransferDto();
+        transferDto.setSenderAccountNumber(senderAccountNumber);
+        model.addAttribute("transferDto", transferDto);
+        return "transfer/new";
+    }
+
+    @PostMapping("/new")
+    public String transferMoney(
+            @Valid @ModelAttribute TransferDto transferDto,
+            BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errorMessage", "Invalid transfer data");
+            model.addAttribute("transferDto", transferDto);
+            return "transfer/new";
+        }
+
+        Money money = new Money(transferDto.getAmount());
+        try {
+            transferService.transferMoney(transferDto.getSenderAccountNumber(), transferDto.getRecipientAccountNumber(), money);
+            return "redirect:/account";
+        } catch (InsufficientFundsException | InvalidAccountNumberException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            model.addAttribute("transferDto", transferDto);
+            return "transfer/new";
         }
     }
 }
--- src/main/java/pl/gm/bankapi/transfer/dto/TransferDto.java
+++ src/main/java/pl/gm/bankapi/transfer/dto/TransferDto.java
@@ -2,12 +2,16 @@ package pl.gm.bankapi.transfer.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
 public class TransferDto {
 
     private String senderAccountNumber;
+    @NotBlank(message = "Recipient account number cannot be blank")
     private String recipientAccountNumber;
+    @DecimalMin(value = "0.01", message = "Amount must be greater than or equal to 0.01")
     private BigDecimal amount;
 }
