effc8c8688b9da24ed86bd266805ad95f730e99f
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-18 20:23:58 +0200

    new impl: @NotNull on amount

--- src/main/java/pl/gm/bankapi/transfer/dto/TransferDto.java
+++ src/main/java/pl/gm/bankapi/transfer/dto/TransferDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
@@ -12,6 +13,7 @@ public class TransferDto {
     private String senderAccountNumber;
     @NotBlank(message = "Recipient account number cannot be blank")
     private String recipientAccountNumber;
+    @NotNull(message = "Amount is required")
     @DecimalMin(value = "0.01", message = "Amount must be greater than or equal to 0.01")
     private BigDecimal amount;
 }
