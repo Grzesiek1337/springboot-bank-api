fdd68f82f48739e08e45ccbff0662d21dcb4eeba
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-19 10:44:57 +0200

    new impl: commentaries

--- src/main/java/pl/gm/bankapi/common/PolishBankAccountGenerator.java
+++ src/main/java/pl/gm/bankapi/common/PolishBankAccountGenerator.java
@@ -1,23 +1,49 @@
 package pl.gm.bankapi.common;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
+/**
+ * This class generates valid Polish bank account numbers according to the standard format.
+ * The account number consists of the two-letter country code ("PL"), a two-digit control number,
+ * an eight-digit bank code, and a sixteen-digit account number.
+ */
+@Component
 public class PolishBankAccountGenerator {
+
+    /** The two-letter country code for Poland */
     private static final String COUNTRY_CODE = "PL";
+
+    /** The length of the bank code */
     private static final int BANK_CODE_LENGTH = 8;
+
+    /** The length of the account number */
     private static final int ACCOUNT_NUMBER_LENGTH = 16;
+
+    /** The possible characters in the account number */
     private static final String[] ACCOUNT_NUMBER_CHARACTERS = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+    /** The random number generator used to generate account numbers */
     private static final Random RANDOM = new Random();
 
+    /**
+     * Generates a complete Polish bank account number.
+     * @return the bank account number as a String
+     */
     public static String generateAccountNumber() {
         StringBuilder sb = new StringBuilder();
-        sb.append(COUNTRY_CODE);
-        sb.append(generateControlNumber());
-        sb.append(generateBankCode());
-        sb.append(generateAccountNumberDigits());
+        sb.append(COUNTRY_CODE); // add country code
+        sb.append(generateControlNumber()); // add control number
+        sb.append(generateBankCode()); // add bank code
+        sb.append(generateAccountNumberDigits()); // add account number digits
         return sb.toString();
     }
 
+    /**
+     * Generates the two-digit control number for the bank account.
+     * @return the control number as a String
+     */
     private static String generateControlNumber() {
         StringBuilder sb = new StringBuilder();
         int controlNumberInt = 98 - generateAccountNumberDigitsMod();
@@ -28,6 +54,10 @@ public class PolishBankAccountGenerator {
         return sb.toString();
     }
 
+    /**
+     * Calculates the modulo 97 of the account number digits, which is used to generate the control number.
+     * @return the modulo 97 as an int
+     */
     private static int generateAccountNumberDigitsMod() {
         String digits = generateAccountNumberDigits();
         int mod = 0;
@@ -38,6 +68,10 @@ public class PolishBankAccountGenerator {
         return mod;
     }
 
+    /**
+     * Generates a random bank code of the specified length using digits 0-9.
+     * @return the bank code as a String
+     */
     private static String generateBankCode() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < BANK_CODE_LENGTH; i++) {
@@ -46,6 +80,10 @@ public class PolishBankAccountGenerator {
         return sb.toString();
     }
 
+    /**
+     * Generates a random account number of the specified length using digits 0-9.
+     * @return the account number as a String
+     */
     private static String generateAccountNumberDigits() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < ACCOUNT_NUMBER_LENGTH; i++) {
