a90bcadcdb5723f8a86d76dcdd9c2b23423ef31a
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-23 21:21:50 +0200

    new refactor

--- src/main/java/pl/gm/bankapi/communication/feedback/repository/FeedBackRepository.java
+++ src/main/java/pl/gm/bankapi/communication/feedback/repository/FeedBackRepository.java
@@ -1,7 +1,9 @@
 package pl.gm.bankapi.communication.feedback.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.gm.bankapi.communication.feedback.model.ContactFeedbackEntity;
 
+@Repository
 public interface FeedBackRepository extends JpaRepository<ContactFeedbackEntity,Long> {
 }
