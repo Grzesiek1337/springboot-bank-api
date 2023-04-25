1b6325c5fc301e805b0bf0b024a766531049f114
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-25 19:35:18 +0200

    new impl: new query methods finding answered and unanswered contacts

--- src/main/java/pl/gm/bankapi/communication/contact/repository/ContactRepository.java
+++ src/main/java/pl/gm/bankapi/communication/contact/repository/ContactRepository.java
@@ -1,9 +1,18 @@
 package pl.gm.bankapi.communication.contact.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.gm.bankapi.communication.contact.model.Contact;
 
+import java.util.List;
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact,Long> {
+
+    @Query("SELECT c FROM Contact c WHERE c.isResponseProvided = false")
+    List<Contact> findAllUnansweredContacts();
+
+    @Query("SELECT c FROM Contact c WHERE c.isResponseProvided = true")
+    List<Contact> findAllAnsweredContacts();
 }
