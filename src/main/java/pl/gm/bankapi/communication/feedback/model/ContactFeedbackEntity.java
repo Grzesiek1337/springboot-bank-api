0c1294b5046f619719dba376e1b46ba269350944
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-25 19:34:29 +0200

    new impl: ContactFeedbackEntity

--- src/main/java/pl/gm/bankapi/communication/feedback/model/ContactFeedbackEntity.java
+++ src/main/java/pl/gm/bankapi/communication/feedback/model/ContactFeedbackEntity.java
@@ -1,13 +1,27 @@
 package pl.gm.bankapi.communication.feedback.model;
 
 import lombok.Data;
+import pl.gm.bankapi.communication.contact.model.Contact;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "contact_feedbacks")
 @Data
 public class ContactFeedbackEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    private String author;
+
+    private LocalDateTime responseTime;
+
+    @OneToOne
+    @JoinColumn(name = "contact_id")
+    private Contact contact;
 }
