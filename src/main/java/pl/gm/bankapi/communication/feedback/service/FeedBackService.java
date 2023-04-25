f4c73099f972be501ff033c92bb74003d337b0ba
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-25 19:38:21 +0200

    new impl: feedback saving method

--- src/main/java/pl/gm/bankapi/communication/feedback/service/FeedBackService.java
+++ src/main/java/pl/gm/bankapi/communication/feedback/service/FeedBackService.java
@@ -1,7 +1,24 @@
 package pl.gm.bankapi.communication.feedback.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import pl.gm.bankapi.communication.feedback.ContactFeedbackDto;
+import pl.gm.bankapi.communication.feedback.model.ContactFeedbackEntity;
+import pl.gm.bankapi.communication.feedback.repository.FeedBackRepository;
 
 @Service
 public class FeedBackService {
+
+    private final FeedBackRepository feedBackRepository;
+    private final ModelMapper modelMapper;
+
+    public FeedBackService(FeedBackRepository feedBackRepository, ModelMapper modelMapper) {
+        this.feedBackRepository = feedBackRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public void saveFeedback(ContactFeedbackDto contactFeedbackDto) {
+        ContactFeedbackEntity feedback = modelMapper.map(contactFeedbackDto, ContactFeedbackEntity.class);
+        feedBackRepository.save(feedback);
+    }
 }
