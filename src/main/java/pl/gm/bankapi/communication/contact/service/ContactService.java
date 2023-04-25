72c3981d59c47610e0b5965809652bd3f80aef12
Grzesiek1337 <gmiedzian@gmail.com>
2023-04-25 19:36:13 +0200

    new impl: new methods finding answered and unanswered contacts, comments

--- src/main/java/pl/gm/bankapi/communication/contact/service/ContactService.java
+++ src/main/java/pl/gm/bankapi/communication/contact/service/ContactService.java
@@ -6,6 +6,10 @@ import pl.gm.bankapi.communication.contact.model.Contact;
 import pl.gm.bankapi.communication.contact.repository.ContactRepository;
 import pl.gm.bankapi.communication.contact.dto.ContactDto;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class ContactService {
 
@@ -17,13 +21,67 @@ public class ContactService {
         this.modelMapper = modelMapper;
     }
 
-    public void saveContact(ContactDto contactDto) {
+    /**
+     * Creates a new contact.
+     */
+    public void createContact(ContactDto contactDto) {
         Contact contact = modelMapper.map(contactDto, Contact.class);
         contact.setResponseProvided(false);
         contactRepository.save(contact);
     }
 
-    public void generateSomeContacts(int number) {
+    /**
+     * Updates an existing contact.
+     */
+    public void updateExistingContact(ContactDto contactDto) {
+        Contact contact = modelMapper.map(contactDto, Contact.class);
+        contactRepository.save(contact);
+    }
+
+    /**
+     * Returns a list of all contacts.
+     */
+    public List<ContactDto> getAllContacts() {
+        List<Contact> contacts = contactRepository.findAll();
+        return contacts.stream()
+                .map(contact -> modelMapper.map(contact, ContactDto.class))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns a list of all unanswered contacts.
+     */
+    public List<ContactDto> findAllUnansweredContacts() {
+        List<Contact> contacts = contactRepository.findAllUnansweredContacts();
+        return contacts.stream()
+                .map(contact -> modelMapper.map(contact, ContactDto.class))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns a list of all answered contacts.
+     */
+    public List<ContactDto> findAllAnsweredContacts() {
+        List<Contact> contacts = contactRepository.findAllAnsweredContacts();
+        return contacts.stream()
+                .map(contact -> modelMapper.map(contact, ContactDto.class))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns the contact with the specified ID.
+     * @throws EntityNotFoundException if the contact is not found
+     */
+    public ContactDto getContactById(Long id) {
+        Contact contact = contactRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Contact not found with id " + id));
+        return modelMapper.map(contact, ContactDto.class);
+    }
+
+    /**
+     * Generates a specified number of dummy contacts for testing purposes.
+     */
+    public void generateDummyContacts(int number) {
         for (int i = 0; i < number; i++) {
             Contact contact = new Contact();
             contact.setName("Little Amy");
