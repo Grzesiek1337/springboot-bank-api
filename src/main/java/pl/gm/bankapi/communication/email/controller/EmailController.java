9b5fed4e712dec8334c5e354041951bdeaca782e
Grzesiek1337 <gmiedzian@gmail.com>
2023-05-03 21:15:48 +0200

    removed unused variable

--- src/main/java/pl/gm/bankapi/communication/email/controller/EmailController.java
+++ src/main/java/pl/gm/bankapi/communication/email/controller/EmailController.java
@@ -1,6 +1,5 @@
 package pl.gm.bankapi.communication.email.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.gm.bankapi.communication.email.service.EmailService;
@@ -10,8 +9,6 @@ import pl.gm.bankapi.communication.email.service.EmailServiceImpl;
 @RequestMapping("/email")
 public class EmailController {
 
-    @Value("${attachment.invoice}")
-    private String attachmentPath;
     private final EmailService emailService;
 
     public EmailController(EmailServiceImpl emailService) {
