72a3c0f92b1cc2cc90e005ab40592cdffbed56d6
Grzesiek1337 <gmiedzian@gmail.com>
2023-05-03 21:14:09 +0200

    new: query finding client data by username

--- src/main/java/pl/gm/bankapi/client/repository/ClientRepository.java
+++ src/main/java/pl/gm/bankapi/client/repository/ClientRepository.java
@@ -1,10 +1,14 @@
 package pl.gm.bankapi.client.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.gm.bankapi.client.model.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
+    @Query("SELECT c FROM Client c JOIN c.user u WHERE u.username = :username")
+    Client getClientByUsername(@Param("username") String username);
 }
