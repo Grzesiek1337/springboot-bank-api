75bff75e0425cc24c62da441e5ad7f25b06b47cb
Grzesiek1337 <gmiedzian@gmail.com>
2023-05-04 21:03:07 +0200

    new: payment dto

---
+++ src/main/java/pl/gm/bankapi/payment/dto/PaymentDto.java
@@ -0,0 +1,18 @@
+package pl.gm.bankapi.payment.dto;
+
+import lombok.Data;
+import pl.gm.bankapi.payment.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class PaymentDto {
+
+    private Long id;
+    private int number;
+    private BigDecimal amount;
+    private LocalDate dueDate;
+    private PaymentStatus status;
+    private PaymentScheduleDto paymentSchedule;
+}
