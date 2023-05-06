a99ed56e03902d9fd1163b32121909b12bee9222
Grzesiek1337 <gmiedzian@gmail.com>
2023-05-06 15:53:27 +0200

    new: notification repository

---
+++ src/main/java/pl/gm/bankapi/notification/repository/NotificationRepository.java
@@ -0,0 +1,16 @@
+package pl.gm.bankapi.notification.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.gm.bankapi.notification.model.NotificationEntity;
+import pl.gm.bankapi.user.model.User;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<NotificationEntity,Long> {
+
+    // Returns all notifications for a given user, ordered by creation date in descending order
+    List<NotificationEntity> findByUserOrderByCreatedAtDesc(User user);
+
+}
