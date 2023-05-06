6e04fb39c7ff868af32f81762738e894e63b06a0
Grzesiek1337 <gmiedzian@gmail.com>
2023-05-06 16:46:42 +0200

    new: validation constraints

--- src/main/java/pl/gm/bankapi/notification/dto/NotificationDto.java
+++ src/main/java/pl/gm/bankapi/notification/dto/NotificationDto.java
@@ -3,6 +3,9 @@ package pl.gm.bankapi.notification.dto;
 import lombok.Data;
 import pl.gm.bankapi.user.dto.UserDto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  Data Transfer Object (DTO) for notifications.
  */
@@ -10,6 +13,8 @@ import pl.gm.bankapi.user.dto.UserDto;
 public class NotificationDto {
 
     private Long id;
+    @NotBlank
+    @Size(max = 200)
     private String message;
     private UserDto userDto;
     private String createdAt;
