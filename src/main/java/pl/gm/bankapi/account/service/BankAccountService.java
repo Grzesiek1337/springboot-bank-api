fa96a0d6d6d5fa2ab97626b5470121cd0bf32668
Grzesiek1337 <gmiedzian@gmail.com>
2023-05-29 21:36:03 +0200

    new: commentaries, get account by id method

--- src/main/java/pl/gm/bankapi/account/service/BankAccountService.java
+++ src/main/java/pl/gm/bankapi/account/service/BankAccountService.java
@@ -6,6 +6,7 @@ import pl.gm.bankapi.account.dto.BankAccountDto;
 import pl.gm.bankapi.account.model.BankAccount;
 import pl.gm.bankapi.account.repository.BankAccountRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,12 @@ public class BankAccountService {
         this.modelMapper = modelMapper;
     }
 
+    /**
+     * Retrieves a list of bank accounts associated with the given client ID.
+     *
+     * @param id the client ID
+     * @return a list of bank account DTOs
+     */
     public List<BankAccountDto> getAccountsByClientId(Long id) {
         List<BankAccount> accounts = bankAccountRepository.getAccountsByClientId(id);
         return accounts.stream()
@@ -27,6 +34,12 @@ public class BankAccountService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Retrieves a list of bank accounts associated with the given username.
+     *
+     * @param username the username
+     * @return a list of bank account DTOs
+     */
     public List<BankAccountDto> getAccountsByUsername(String username) {
         List<BankAccount> accounts = bankAccountRepository.getAccountsByUsername(username);
         return accounts.stream()
@@ -34,6 +47,16 @@ public class BankAccountService {
                 .collect(Collectors.toList());
     }
 
-
-
+    /**
+     * Retrieves a bank account with the given ID.
+     *
+     * @param id the bank account ID
+     * @return the bank account DTO
+     * @throws EntityNotFoundException if the bank account is not found
+     */
+    public BankAccountDto getAccountById(Long id) {
+        BankAccount bankAccount = bankAccountRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Entity not found with ID: " + id));
+        return modelMapper.map(bankAccount, BankAccountDto.class);
+    }
 }
