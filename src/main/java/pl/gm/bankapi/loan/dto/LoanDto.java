d7fc9224bd1edd4ebc177b1e91a4e8322765294a
Grzesiek1337 <gmiedzian@gmail.com>
2023-05-29 21:46:09 +0200

    refactor

--- src/main/java/pl/gm/bankapi/loan/dto/LoanDto.java
+++ src/main/java/pl/gm/bankapi/loan/dto/LoanDto.java
@@ -1,20 +1,20 @@
 package pl.gm.bankapi.loan.dto;
 
 import lombok.Data;
-import pl.gm.bankapi.payment.dto.PaymentScheduleDto;
+import lombok.experimental.Accessors;
 
 import java.math.BigDecimal;
 
 /**
  * A data transfer object representing a loan.
  */
-@Data public class LoanDto {
+@Data
+@Accessors(chain = true)
+public class LoanDto {
 
     private Long id;
     private String owner;
     private BigDecimal amount;
-    private BigDecimal remainsToPaid
-    ;private BigDecimal monthlyPayments;
+    private BigDecimal remainsToPaid;
     private int paymentTerms;
-    private PaymentScheduleDto paymentScheduleDto;
 }
